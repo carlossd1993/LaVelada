7797312853e986a8b85c44c5d6610a63ae3c209b
carlossd1993 <161085364+carlossd1993@users.noreply.github.com>
2024-04-18 19:19:02 +0200

    creada página de error y modificado el navbar para enlazar las rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomeComponent} from "./pages/home/home.component";
+import {ErrorComponent} from "./pages/error/error.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: "home", component: HomeComponent},
+  {path: "combates", component: ErrorComponent},
+  {path: "pronosticos", component: ErrorComponent},
+  {path: "entradas", component: ErrorComponent},
+  {path: "**",component: HomeComponent}
+];
